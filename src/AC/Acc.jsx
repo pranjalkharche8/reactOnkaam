5e5c5da958e63cee95fe829e6dc7886608c066e0
pranjal <pranjalkharche134@gmail.com>
2024-10-17 14:50:26 +0530

    By pranjal

--- src/AC/Acc.jsx
+++ src/AC/Acc.jsx
@@ -7,6 +7,13 @@ import Homepage from "../HomePage.jsx";
 import Itemfunction from '../Booking/items.jsx';
 import Sample from "../assets/acweb.webp";
 import Linee from '../Booking/line.jsx';
+import Power from "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fwe-can-handle-your-hvac-needs-air-conditioner-repair-clipart-400x422-png-clipart-download-clipartmaxcom-in-2022--644296290458769552%2F&psig=AOvVaw2AiQFdOsJMA94pu7Lt6I6T&ust=1729234192498000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKiZ_8HplIkDFQAAAAAdAAAAABAE";
+import Anti from "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fcleaning-cartoon&psig=AOvVaw0Rv8s4PLx8ZW3W1sdG8-D8&ust=1729234421648000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOi22s7qlIkDFQAAAAAdAAAAABAE";
+import Lite from "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Finstallation-ac%3Fpage%3D8&psig=AOvVaw0A50VPxxF5-w87G6LlW_bW&ust=1729234566227000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJCt3PLqlIkDFQAAAAAdAAAAABAE";
+import AC1 from "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fvector%2Fprofessional-air-conditioners-service-and-repair-gm1194483830-340143320&psig=AOvVaw1ohupjypI9sAjmRIZ0Iv04&ust=1729234614360000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCICQ1IvrlIkDFQAAAAAdAAAAABAE";
+import AC2 from "https://www.google.com/url?sa=i&url=https%3A%2F%2Facesindia.in%2Fproducts%2Fac-gas-leak-fix-refill-service&psig=AOvVaw3kXXwwguVhLF107aj01QKf&ust=1729234666477000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJC46aTrlIkDFQAAAAAdAAAAABAE";
+import ACI from "https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonscout.com%2Ffree-lottie-animation%2Fac-installation-9617085&psig=AOvVaw128x6ZSlXY1AiS9CbPwArG&ust=1729234736962000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLDAtNTrlIkDFQAAAAAdAAAAABAE";
+import ACU from "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dbhomeappliance.in%2Fair-conditioner%2Fac-uninstallation&psig=AOvVaw2HsOXqnD_YvxIzPRY6OuYq&ust=1729234819524000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCp0frrlIkDFQAAAAAdAAAAABAE";
 export default function Booking() {
     let sty = { fontWeight: 900, marginTop: "0.8rem", marginBottom: "1.5rem", fontSize: "28px" };
     let stye = { fontWeight: 900 };
@@ -23,31 +30,31 @@ export default function Booking() {
                 <Breakline></Breakline>
                 <div>
                     <h3 style={sty}>Switch & Socket</h3>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="59" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="power saver AC service" rating="4.8 (16k reviews)" prices="599" image={Power}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="56" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="Anti-rust deep clean AC service" rating="4.8 (16k reviews)" prices="899" image={Anti}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="52" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="Lite AC service" rating="4.8 (16k reviews)" prices="499" image={Lite}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="512" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="AC repair" rating="4.8 (16k reviews)" prices="299" image={AC1}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="59657" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="AC Gas leak fix &refill" rating="4.8 (16k reviews)" prices="2500" image={AC2}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="593453" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="AC installation" rating="4.8 (16k reviews)" prices="799" image={ACI}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="5879" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="AC uninstallation" rating="4.8 (16k reviews)" prices="499" image={ACU}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
--- src/Salons/Salons.jsx
+++ src/Salons/Salons.jsx
@@ -7,6 +7,7 @@ import Homepage from "../HomePage.jsx";
 import Itemfunction from '../Booking/items.jsx';
 import Sample from "../assets/acweb.webp";
 import Linee from '../Booking/line.jsx';
+import Linee from "";
 export default function Booking() {
     let sty = { fontWeight: 900, marginTop: "0.8rem", marginBottom: "1.5rem", fontSize: "28px" };
     let stye = { fontWeight: 900 };
@@ -18,40 +19,172 @@ export default function Booking() {
                     <Link to="/">
                         <i class="fa-solid fa-arrow-left fa-lg"></i>
                     </Link>
-                    <h3 style={stye}>Men and Women Salons</h3>
+                    <h2 style={stye}>Men and Women Salons</h2>
                 </div>
                 <Breakline></Breakline>
                 <div>
-                    <h3 style={sty}>Switch & Socket</h3>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="59" image={Sample}></Itemfunction>
+                    <h3 style={sty}>BestSeller Packages</h3>
+                    <Itemfunction Itemname="Full chocolate wax" rating="4.8 (16k reviews)" prices="949" image={Sample}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="56" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="Honey Waxing" rating="4.8 (16k reviews)" prices="649" image={Sample}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="52" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="Fcae wax" rating="4.8 (16k reviews)" prices="59" image={Sample}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="512" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="Waxing , Facial & Face wax" rating="4.8 (16k reviews)" prices="2449" image={Sample}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="59657" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="waxing & Hair care" rating="4.8 (16k reviews)" prices="999" image={Sample}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="593453" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="Manicure & Pedicure" rating="4.8 (16k reviews)" prices="1500" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <Breakline></Breakline>
+                <div>
+                    <h3 style={sty}>Make your Package</h3>
+                    <Itemfunction Itemname="Customise package " rating="4.8 (16k reviews)" prices="2600" image={Sample}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="5879" image={Sample}></Itemfunction>
+                    <h3 style={sty}>Waxing</h3>
+                    <Itemfunction Itemname="Full Arms" rating="4.8 (16k reviews)" prices="299" image={Sample}></Itemfunction>
                     <Linee></Linee>
                 </div>
                 <div>
-                    <Itemfunction Itemname="Switch Replacement" rating="4.8 (16k reviews)" prices="5797" image={Sample}></Itemfunction>
+                    <Itemfunction Itemname="Half Arms" rating="4.8 (16k reviews)" prices="199" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Full legs" rating="4.8 (16k reviews)" prices="359" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="half leg" rating="4.8 (16k reviews)" prices="180" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Underarms" rating="4.8 (16k reviews)" prices="49" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Bikini wax" rating="4.8 (16k reviews)" prices="999" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Back wax" rating="4.8 (16k reviews)" prices="449" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Stomach wax" rating="4.8 (16k reviews)" prices="299" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Bikini line wax" rating="4.8 (16k reviews)" prices="249" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <h3 style={sty}>Manicure & Pedicure</h3>
+                    <Itemfunction Itemname="Elysian British rose pedicure" rating="4.8 (16k reviews)" prices="699" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Elysian chocolate & venilla pedicure" rating="4.8 (16k reviews)" prices="959" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Foot massage" rating="4.8 (16k reviews)" prices="199" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Cut , File & polish" rating="4.8 (16k reviews)" prices="1049" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Elysian British rose menicure" rating="4.8 (16k reviews)" prices="699" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="cut , file & polish" rating="4.8 (16k reviews)" prices="249" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Elysian chocolate & venilla manicure" rating="4.8 (16k reviews)" prices="899" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="crayon manicure" rating="4.8 (16k reviews)" prices="799" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <h3 style={sty}>Facial & Cleanup</h3>
+                    <Itemfunction Itemname="Glovite lightening facial" rating="4.8 (16k reviews)" prices="1099" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="O3+shine & glow" rating="4.8 (16k reviews)" prices="1699" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Elysian firming wine glow" rating="4.8 (16k reviews)" prices="1199" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="O3+ power brightening facial" rating="4.8 (16k reviews)" prices="1999" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Sara fruit cleanup" rating="4.8 (16k reviews)" prices="699" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="O3+ tan clear cleanup" rating="4.8 (16k reviews)" prices="899" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="O3+ feel youthful facial" rating="4.8 (16k reviews)" prices="1599" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <h3 style={sty}>Hair care</h3>
+                    <Itemfunction Itemname="Head massage " rating="4.8 (16k reviews)" prices="349" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Hair colour / mahandi" rating="4.8 (16k reviews)" prices="299" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Loreal hair spa" rating="4.8 (16k reviews)" prices="1249" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Hair wash" rating="4.8 (16k reviews)" prices="499" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <h3 style={sty}>Bleach & detan</h3>
+                    <Itemfunction Itemname="Detan " rating="4.8 (16k reviews)" prices="399" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Bleach" rating="4.8 (16k reviews)" prices="349" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <h3 style={sty}>Threading & Face waxing</h3>
+                    <Itemfunction Itemname="Threading & Face waxing " rating="4.8 (16k reviews)" prices="79" image={Sample}></Itemfunction>
+                    <Linee></Linee>
+                </div>
+                <div>
+                    <Itemfunction Itemname="Face waxing" rating="4.8 (16k reviews)" prices="69" image={Sample}></Itemfunction>
                     <Linee></Linee>
                 </div>
             </div>
